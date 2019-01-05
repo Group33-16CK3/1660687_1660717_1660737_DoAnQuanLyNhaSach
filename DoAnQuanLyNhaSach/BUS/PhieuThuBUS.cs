3f12a991eca48e723f38f443565e207378b89c25
Tuan <tramleanhtuan1111@gmail.com>
2019-01-06 03:54:13 +0700

    Commit 3

---
+++ DoAnQuanLyNhaSach/BUS/PhieuThuBUS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoAnQuanLyNhaSach.DAO;
+using DoAnQuanLyNhaSach.DTO;
+
+namespace DoAnQuanLyNhaSach.BUS
+{
+    class PhieuThuBUS
+    {
+        PhieuThuDAO ptDAO = new PhieuThuDAO();
+        public  bool ThemPhieuThu(PhieuThuDTO pt)
+        {
+            if (ptDAO.GetPhieuThuByMa(pt.MaPhieuThu) == null)
+            {
+                ptDAO.Insert(pt);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public  bool XoaPhieuThu(PhieuThuDTO pt)
+        {
+            if (ptDAO.GetPhieuThuByMa(pt.MaPhieuThu) != null)
+            {
+                ptDAO.Delete(pt);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public  bool SuaPhieuThu(PhieuThuDTO pt)
+        {
+            if (ptDAO.GetPhieuThuByMa(pt.MaPhieuThu) != null)
+            {
+                ptDAO.Update(pt);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- DoAnQuanLyNhaSach/DAO/PhieuThuDAO.cs
+++ DoAnQuanLyNhaSach/DAO/PhieuThuDAO.cs
@@ -57,5 +57,82 @@ namespace DoAnQuanLyNhaSach.DAO
             }
             return flag;
         }
+        public void Update(PhieuThuDTO pt)
+        {
+            try
+            {
+                string sql = "Update  PHIEUTHU set NgayThu =('" + pt.NgayThu + "'),SoTienThu=(" + pt.SoTienThu + ") where MaPhieuThu=" + pt.MaPhieuThu + "";
+                kn.Connect();
+                kn.ExecuteNonQuery(CommandType.Text,sql, new SqlParameter { ParameterName = "MaPhieuThu", Value = pt.MaPhieuThu },
+                    new SqlParameter { ParameterName = "NgayThu", Value = pt.NgayThu },
+                    new SqlParameter { ParameterName = "SoTienThu", Value = pt.SoTienThu });
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            
+            }
+            finally
+            {
+                kn.Disconnect();
+            }
+        }
+        public bool Delete(PhieuThuDTO pt)
+        {
+            try
+            {
+                string sql = "delete from PHIEUTHU where MaPhieuThu= " + pt.MaPhieuThu + "";
+                kn.Connect();
+                kn.ExecuteNonQuery(CommandType.Text, sql, new SqlParameter { ParameterName = "MaPhieuThu", Value = pt.MaPhieuThu });
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                kn.Disconnect();
+            }
+        }
+        public  PhieuThuDTO GetPhieuThuByMa(string Ma)
+        {
+            string sql = "select * from PHIEUTHU where MaPhieuThu=" + Ma + "";
+            kn.Connect();
+            DataTable dt = kn.Select(CommandType.Text, sql, new SqlParameter { ParameterName = "MaPhieuThu", Value = Ma });
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                PhieuThuDTO pt = new PhieuThuDTO();
+                pt.MaPhieuThu = dt.Rows[0].ItemArray[0].ToString();
+                               
+                return pt;
+            }
+
+        }
+        public  DataTable GetPhieuThuCondition(string MaPhieuThu)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "select * from PHIEUTHU,KHACHHANG where MaPhieuThu = " + MaPhieuThu + "";
+                kn.Connect();
+                dt = kn.Select(CommandType.Text, sql, new SqlParameter { ParameterName = "MaPhieuThu", Value = MaPhieuThu });
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }            
+            finally
+            {
+                kn.Disconnect();
+            }
+            return dt;
+        }
     }
 }
