c949410e77f6d5aa04e95cc3080a051c1d9d49c7
Tuan <tramleanhtuan1111@gmail.com>
2019-01-06 23:15:07 +0700

    commit 6

--- DoAnQuanLyNhaSach/Program.cs
+++ DoAnQuanLyNhaSach/Program.cs
@@ -17,7 +17,7 @@ namespace DoAnQuanLyNhaSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
         // abc
     }
